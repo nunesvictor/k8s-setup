d1abc78179dc020b5ebae8ebf87a9adca5f87798
Victor Nunes <nunessvictorr@gmail.com>
2020-02-28 21:38:13 -0300

    Create k8s-setup.sh

---
+++ k8s-setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# disable swap
+sudo swapoff -a
+sudo sed -e '/swap/s/^/#/g' -i /etc/fstab
+
+# ensure legacy binaries are installed
+sudo apt-get update && \
+sudo apt-get install -y iptables arptables ebtables
+
+# switch to legacy versions
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+sudo update-alternatives --set arptables /usr/sbin/arptables-legacy
+sudo update-alternatives --set ebtables /usr/sbin/ebtables-legacy
+
+# install curl
+sudo apt-get install curl
+
+# install docker
+curl -fsSL https://test.docker.com | bash
+
+# install kubeadm, kubelet and kubectl
+sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl

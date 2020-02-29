94d217b0e5777c7da1b45736afb6328e0bbaab86
Victor Nunes <nunessvictorr@gmail.com>
2020-02-28 21:42:42 -0300

    Update k8s-setup.sh

--- k8s-setup.sh
+++ k8s-setup.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
+# 
+# Install docker and kubernetes tools
+# run as root with curl -fsSL k8s-setup.sh | bash
 
 # disable swap
-sudo swapoff -a
-sudo sed -e '/swap/s/^/#/g' -i /etc/fstab
+swapoff -a
+sed -e '/swap/s/^/#/g' -i /etc/fstab
 
 # ensure legacy binaries are installed
-sudo apt-get update && \
-sudo apt-get install -y iptables arptables ebtables
+apt-get update && \
+apt-get install -y iptables arptables ebtables
 
 # switch to legacy versions
-sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
-sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-sudo update-alternatives --set arptables /usr/sbin/arptables-legacy
-sudo update-alternatives --set ebtables /usr/sbin/ebtables-legacy
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+update-alternatives --set arptables /usr/sbin/arptables-legacy
+update-alternatives --set ebtables /usr/sbin/ebtables-legacy
 
 # install apt-transport-https and curl
-sudo apt-get install -y apt-transport-https curl
+apt-get install -y apt-transport-https curl
 
 # install docker
 curl -fsSL https://test.docker.com | bash
 
 # install kubeadm, kubelet and kubectl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
